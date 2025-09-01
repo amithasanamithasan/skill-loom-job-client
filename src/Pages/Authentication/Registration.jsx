5004489f3eb9691337bb09dd3ab02dbc7bb189d1
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-09-01 21:23:09 +0600

    Fiexd

--- src/Pages/Authentication/Registration.jsx
+++ src/Pages/Authentication/Registration.jsx
@@ -119,7 +119,10 @@ const Registration = () => {
             <div className='mt-6'>
               <button
                 type='submit'
-                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+                className='w-full px-6 py-3 text-sm font-medium tracking-wide
+                 text-black capitalize transition-colors duration-300 
+                 transform bg-gray-300 rounded-lg hover:bg-gray-400
+                  focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
               >
                 Sign Up
               </button>
