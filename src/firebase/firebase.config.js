1f4f30ced5b66bdf9c01d2d4b043bc985d3821b0
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-09-02 10:06:22 +0600

    Firebase Auth setup done with context api using main.jsx

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBBmTJM2JTEDIW2BlBz7qtUbDuJBJ1-2C4",
+    authDomain: "skill-loom-job-client.firebaseapp.com",
+    projectId: "skill-loom-job-client",
+    storageBucket: "skill-loom-job-client.firebasestorage.app",
+    messagingSenderId: "753409067388",
+    appId: "1:753409067388:web:2198c5ea4482c3ac3e3cf9"
+};
+
+
+export const app = initializeApp(firebaseConfig);
